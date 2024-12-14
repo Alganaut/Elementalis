a58292a12a9e1fc4ee441a44c96f8693e19cef0d
Alganaut <emretug624@gmail.com>
2024-12-14 23:44:14 +0000

    Added Morose bush generation (early)

--- src/main/java/com/Alganaut/Elementalis/datagen/ModDatapackProvider.java
+++ src/main/java/com/Alganaut/Elementalis/datagen/ModDatapackProvider.java
@@ -2,18 +2,27 @@ package com.Alganaut.Elementalis.datagen;
 
 import com.Alganaut.Elementalis.Elementalis;
 import com.Alganaut.Elementalis.registry.trim.ModTrimPatterns;
+import com.Alganaut.Elementalis.registry.worldgen.ModBiomeModifiers;
+import com.Alganaut.Elementalis.registry.worldgen.ModConfiguredFeatures;
+import com.Alganaut.Elementalis.registry.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
-public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-        .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap);
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap)
+
+            .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(Elementalis.MODID));
